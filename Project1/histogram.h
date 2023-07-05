5faa0ba54f94b1df4eae93710b4c1df7e599fc53
Vadim (VadimZA) <lecraftmc.kick@gmail.com>
2023-07-06 01:52:56 +0300

    module: добавлено решение домашнего задания

--- Project1/histogram.h
+++ Project1/histogram.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector> 
+#include <algorithm>
 #include <string> 
 
 using namespace std;
@@ -10,4 +11,7 @@ void svg_begin(double width, double height);
 void svg_end();
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x = 0, double y = 0, double width = 100, double height = 200, string stroke = "black", string fill = "black");
-void show_histogram_svg(const vector<size_t>& bins);
+void show_histogram_svg(const vector<size_t>& bins, string column_color);
+bool check_color(string color);
+string input_color_svg();
+void svg_header(const size_t width, const size_t baseline, string text);
