291d72cf82fb3c0042dcd88c494aedc113e506d1
Vadim (VadimZA) <lecraftmc.kick@gmail.com>
2023-07-06 01:37:13 +0300

    module: добавлен хедер для SVG функций

--- Project1/histogram.h
+++ Project1/histogram.h
@@ -1,5 +1,13 @@
 #pragma once
-#include <vector>
+#include <iostream>
+#include <vector> 
+#include <string> 
+
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max);
+void svg_begin(double width, double height);
+void svg_end();
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x = 0, double y = 0, double width = 100, double height = 200, string stroke = "black", string fill = "black");
+void show_histogram_svg(const vector<size_t>& bins);
